5da590315f2467c4938de388cc30cef7ae491e1f
Shadows <36662635+RoyalBladeV2@users.noreply.github.com>
2020-09-17 22:00:20 +0300

    First commit

---
+++ src/main/java/com/example/Week2Bookstore/Week2BookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.Week2Bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week2BookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week2BookstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Week2Bookstore/Week2BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Week2Bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week2BookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
